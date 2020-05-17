ab88ceea361689479f1c2d2eac57cdf481beb15f
Michael Weiner <vikings412@gmail.com>
2020-05-17 14:53:43 -0500

    tagging  v2.0 for release

--- README.md
+++ README.md
@@ -1,39 +1,23 @@
 # csv_report_generator
-Generate a custom CSV file with data that you select from every row within an existing CSV file. 
+Generate a new, custom CSV file with data that you select from every row within an existing CSV file. 
 
 ## About this Python Script
-Have you ever had a complex CSV files, but you only wanted certain columns of data from every row within that CSV file? This tool allows you to read in only CSV files and select what columns of data you want pulled from the CSV file that will be placed in a new CSV file that will be generated. **(Note: You can only read in CSV files.)** 
+Do you deal with large CSV files where you often only need certain columns of data for every row within the file? This tool allows you to read in **only** CSV files and select what columns of data you want pulled from the CSV file that will be placed in a new CSV file that will be generated.
 
 ## Script Requirements
-* Python 3 - The machine on which you intend to use this script will need Python 3 installed. (Any version of Python 3 will work. The lastest, stable build is prefered. 
-* CSV File - For the program to exectue, you will need a CSV file stored on the machine that will run the script. 
+* Python 3 - The machine on which you intend to use this script will need Python 3 installed. (Any version of Python 3 will work. The latest, stable build is preferred. 
+* CSV File - For the program to execute, you will need a CSV file stored on the machine that will run the script. 
 
 ## How to Use this Script
 1. Open your machine's terminal or command prompt and navigate to the directory where you have stored this script. 
 1. Within the directory, run the command `python3 -i csv_report_builder.py` **(Note: This will open the script file in interactive mode that will allow the GUI to open.)**
 1. Click on the "Select a CSV File" button to open a file picker dialog. Select the CSV that you would like to pull data from.
 1. Click on the "Select a Directory" button to open a directory picker dialog. Select a directory where you would like the new CSV file that will be generated to be saved. 
-  **(Note: The output file will be nammed** `output_<todays_date>_<name_of_your_csv_file>` **where <todays_date> is the date that the report is generated in the form of YYYY-MM-DD HH-MM-SS and <name_of_your_csv_file> is the exact file name of the CSV file you read in.)**
-1. A list of all of the columns and their index numbers will be displayed under "<-- Columns within this CSV File -->". In the textbox outlined with a black border to the right of this list, enter the index numbers of each column that you would like included in your output file separated by commas. **(Note: You must separate each number by a comma and the order in which you enter the column numbers matters. See the [example scenario](https://github.com/mike-weiner/csv_report_generator/tree/master#exampel-scenario) below. )**
-1. Once you have entered the desired data that will be generated in a new CSV file, click the "Generate Report" button. 
+  **(Note: The output file will be named** `output_<todays_date>_<name_of_your_csv_file>` **where <todays_date> is the date that the report is generated in the form of YYYY-MM-DD HH-MM-SS and <name_of_your_csv_file> is the exact file name of the CSV file you read in.)**
+1. A list of all of the columns within the CSV file you selected will be displayed under "Columns within CSV File". Click to select or deselect the names of the columns that you would like in the new CSV file that will be generated.
+1. Once you have selected the desired columns that will be generated in a new CSV file, click the "Generate Report" button. 
 1. Open a file explorer on your machine, navigate to the directory you specified in Step #4 and open the newly generated CSV file. 
 1. Click the "Close Program" button to close the GUI and exit the Python script.
-
-## Exampel Scenario
-If you had a CSV file that had a person's Prefix in index 0 (`[0]`), a person's First Name in index 1 (`[1]`), a person's Middle Name in index 2 (`[2]`), a person's Last Name in index 3 (`[3]`), and their Email Address in index 4 (`[4]`) entering `1,3,4` in the textbox would create a CSV file at the directory your specified with the columns `First Name, Last Name, and Email` (in that order).
-
-Entering `4,3,1` in the textbox would create a CSV file at the directory your specified with the columns `Email, Last Name, First Nmae` (in that order).
-
-Entering `3,2,1` in the textbox would create a CSV file at the directory your specified with the columns `Last Name, Middle Name, First Nmae` (in that order).
-
-Entering `3,1` in the textbox would create a CSV file at the directory your specified with the columns `Last Name, First Nmae` (in that order).
-
-Entering `0, 1` in the textbox would create a CSV file at the directory your specified with the columns `Prefix, First Nmae` (in that order).
-
-As you can see, index numbers can be entered in any order as long as they are separated by a comma. **(Note: Entering anything other than numbers separted by a comma will result in an error.)**
-
-## Common Errors
-* Entering anything other than numbers separted by commas in the textbox wtih a black border will result in an error. To fix this, please remove anything other than a combination of numeric values that are listed under `<-- Columns within this CSV File -->` separated by commas. (See the [example scenario](https://github.com/mike-weiner/csv_report_generator/tree/master#exampel-scenario) for further explanation and examples.) 
 
 ## Image Assets
 Below is a screenshot of that this script's GUI looks like before opening a CSV to read data from. 
--- csv_report_builder.py
+++ csv_report_builder.py
@@ -5,14 +5,14 @@ import tkinter as tk
 from datetime import datetime
 from tkinter import filedialog
 from tkinter import font
+from tkinter import *
 
 # Create GUI window and set window title
 builder_win = tk.Tk()
 builder_win.title("CSV Report Builder")
 
 # Set minimum window size
-builder_win.minsize(200, 200)
-
+builder_win.minsize(200, 400)
 
 # Create function to allow user to select file
 def select_file():
@@ -34,21 +34,12 @@ def select_file():
             csv_file_reader = csv.reader(csv_file)
             data = [row for row in csv_file_reader]
 
-        # Get the headers in the CSV files to display next to user input field
-        header_label = ''
-
         # Add all of the differnet headers in the CSV file to the label for the user to see
         for index, col in enumerate(data[0]):
-            header_label += '[' + str(index) + '] --> ' + col + '\n'
-
-        header_label.strip()
-
-        # Set the text displayed in the label to all of the CSV header columns
-        csv_columns_label['text'] = header_label
+            csv_col_select_listbox.insert(index, col)
 
         # Change the button to allow the user to select an output directory
         select_file_output_path_button['state'] = 'normal'
-
     
 # Create function to allow user to select destination directory
 def select_directory():
@@ -59,12 +50,8 @@ def select_directory():
                                                   title = "Select a Directory")
 
     if builder_win.dirpath != '':
-        # Change the text field for the user to input a list required columns
-        user_column_headers['state'] = 'normal'
-
         # Change the generate report button to normal to allow the user to generate their report
         generate_report_button['state'] = 'normal'
-
 
 # Create a function to generate a report in the specified directory
 def create_report():
@@ -73,8 +60,8 @@ def create_report():
     name_of_output = "output_" + str(datetime.now()) + '_' + builder_win.filename
     complete_name_of_output = os.path.join(builder_win.dirpath, name_of_output + '.csv')
 
-    # Get the user's input from the entry box
-    user_entry = user_column_headers.get("1.0", "end-1c")
+    # Get the user's selection(s) from the Listbox
+    user_entry = csv_col_select_listbox.curselection()
 
     # Create a file with the generated filename
     file_output = open(complete_name_of_output, "w")
@@ -82,8 +69,8 @@ def create_report():
     # Initialize a variable to store the string that is to be written to the new file
     string_to_write_to_file = ''
 
-    # Split user entry by comma
-    user_entry_list = user_entry.split(',')
+    # Convert tuple of user selections from tuple into list
+    user_entry_list = list(user_entry)
 
     # Open given file
     with open(builder_win.filepath) as csv_file:
@@ -93,11 +80,13 @@ def create_report():
     # For every row in the CSV file, select the columns specificed by the user
     for indx_row, row in enumerate(data):
         for indx_col, user_index in enumerate(user_entry_list):
+
             # If this is the last column in the row, don't add a comma at the end
             if indx_col == len(user_entry_list) - 1:
                 string_to_write_to_file += row[int(user_index)]
             else:
                 string_to_write_to_file += row[int(user_index)] + ','
+
         # If this is the last row in the CSV file, don't add a new line
         if indx_row != len(data) - 1:
             string_to_write_to_file += '\n'
@@ -109,42 +98,44 @@ def create_report():
     file_output.write(string_to_write_to_file)
     file_output.close()
 
-
 # Create label and button for selecting a CSV file
-select_file_path_label = tk.Label(builder_win, text = "1. Selct a CSV File")
-select_file_path_label.grid(row = 0, column = 0, columnspan = 2, sticky='nesw', padx = 15)
+select_file_path_label = tk.Label(builder_win, text = "1. Select a CSV File", bg="#5a5a5a", fg="#ffffff", font="-weight bold", padx = 15)
+select_file_path_label.grid(row = 0, column = 0, columnspan = 2, sticky='nesw')
 
 select_file_path_button = tk.Button(builder_win, text = "Select a CSV File", command = select_file)
 select_file_path_button.grid(row = 1, column = 0, columnspan = 2, sticky='nesw', padx = 15)
 
 # Create label and button for selecting output location of report
-select_file_output_path_label = tk.Label(builder_win, text = "2. Select an Output Location for Your Report", wraplength = 200)
-select_file_output_path_label.grid(row = 2, column = 0, columnspan = 2, sticky='nesw', padx = 15, pady = (15,0))
+select_file_output_path_label = tk.Label(builder_win, text = "2. Select Save Location", bg="#5a5a5a", fg="#ffffff", font="-weight bold", padx = 15)
+select_file_output_path_label.grid(row = 2, column = 0, columnspan = 2, sticky='nesw', pady = (15,0))
 
 select_file_output_path_button = tk.Button(builder_win, text = "Select a Directory", command = select_directory, state = "disabled")
 select_file_output_path_button.grid(row = 3, column = 0, columnspan = 2, sticky='nesw', padx = 15)
 
-# Create label to display create report
-generate_report_label = tk.Label(builder_win, text = "3. Generate a Custom Report. \n Enter the number of each column of data (separted by comma) in the black box below that you would like in your new report. Order matters.", wraplength = 300)
-generate_report_label.grid(row = 4, column = 0, columnspan=2, sticky='nesw', padx = 15, pady = (15,15))
+# Create label for selecting desired column section
+generate_report_label = tk.Label(builder_win, text = "3. Select Desired Columns", bg="#5a5a5a", fg="#ffffff", font="-weight bold", padx = 15)
+generate_report_label.grid(row = 4, column = 0, columnspan=2, sticky='nesw', pady = (15,0))
 
-# Create label to display CSV file headers
-csv_columns_label = tk.Label(builder_win, text="<-- Columns within this CSV File -->\n")
-csv_columns_label.grid(row = 5, column = 0, sticky='nesw', padx = 15)
+# Create label to display CSV headers
+csv_columns_label = tk.Label(builder_win, text="Columns within CSV File", font="-weight bold", padx = 15)
+csv_columns_label.grid(row = 5, column = 0, sticky='nesw', pady = (15,0))
 
-# Create entry to allow for user input for output file
-user_column_headers = tk.Text(width = 20, height = 1, borderwidth = 2, relief = "solid", state = "disabled")
-user_column_headers.grid(row = 5, column = 1, sticky='nesw', padx = 15)
+# Create label to instruct users about how to select CSV headers
+csv_columns_label = tk.Label(builder_win, text="Select/deselect CSV columns to include in the new report by clicking on the titles below. Click the `Generate Report` button below to create the new CSV file with the selected columns.", wraplength = 200, padx = 15)
+csv_columns_label.grid(row = 6, column = 0, sticky='nesw', pady = (0, 10))
+
+# Establish Listbox to allow user to select desired CSV columns for output report
+csv_col_select_listbox = Listbox(builder_win, selectmode = MULTIPLE, exportselection = 0, width = 0, height = 0)
+csv_col_select_listbox.grid(row = 7, column = 0, sticky='nesw', padx = 15, pady = (0,15))
+
+# Create label for generating report section
+generate_report_label = tk.Label(builder_win, text = "4. Generate New CSV", bg="#5a5a5a", fg="#ffffff", font="-weight bold", padx = 15)
+generate_report_label.grid(row = 8, column = 0, columnspan=2, sticky='nesw', pady = (15,0))
 
 # Create a button to generate the report
 generate_report_button = tk.Button(builder_win, text = "Generate Report", command = create_report, state = "disabled")
-generate_report_button.grid(row = 6, column = 0, columnspan = 2, sticky='nesw', padx = 15, pady = (15,0))
-
-# Create a custom font for the 'Close Program' button
-close_pro_button_font = font.Font(weight = 'bold')
+generate_report_button.grid(row = 9, column = 0, columnspan = 2, sticky='nesw', padx = 15)
 
 # Create a button to close the program
-close_program_button = tk.Button(builder_win, text = "Close Program", command = quit)
-close_program_button['font'] = close_pro_button_font
-close_program_button.grid(row = 7, column = 0, columnspan = 2, sticky='nesw', pady = 7.5, padx = 15)
-
+close_program_button = tk.Button(builder_win, text = "Close Program", command = quit, font="-weight bold")
+close_program_button.grid(row = 10, column = 0, columnspan = 2, sticky='nesw', pady = 7.5, padx = 15)
