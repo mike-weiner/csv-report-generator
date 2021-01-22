0b60cfd987e3e27dedbf1984790dcd3dd6b81abd
Michael Weiner <vikings412@gmail.com>
2021-01-22 12:30:52 -0600

    clean code style and update README

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ Do you deal with large CSV files where you often only need certain columns of da
 ## How to Use this Script
 1. Open your machine's terminal or command prompt and navigate to the directory where you have stored this script. 
 1. Within the directory, place the CSV you would like to pull data from.
-1. Run the command `python3 csv_report_builder.py [Filename of Original CSV] [Column Names (Separated By Comma)]`
+1. Run the command `python3 csv_report_builder.py filename colnames`
 
-* Note: `[Filename of Original CSV]` is the full name of a CSV file such as: `trees.csv`
-* Note: `[Column Names (Separated By Comma)]` is a list of the column names you would like in your new CSV file (order matters). (i.e. `name,index,date`)
+* Note: `filename` is the full name of a CSV file such as: `trees.csv`
+* Note: `colnames` is a list of the column names you would like in your new CSV file (order matters). (i.e. `name,index,date`)
 
 # Example 
 For example, assume I have a CSV file titled `trees.csv` in the same directory that I have the `csv_report_builder.py` script in. The `trees.csv` file has the following column names in the header row:
@@ -27,4 +27,4 @@ I want a new CSV file with the only these columns in this order:
 | "Volume(ft^3)" | "Girth (in)" | Index |
 | -------------- | ------------ | ----- |
 
-To accomplish this, I would run the command `python3 csv_report_builder.py trees.csv "Volume(ft^3)","Girth (in)",Index`. This will create a new file in the same directory with the specified columns in the specified order.
+To accomplish this, I would run the command `python3 csv_report_builder.py trees.csv "Volume(ft^3)","Girth (in)",Index`. This will create a new file in the same directory with the specified columns in the specified order titled `output-YYYY-mm-dd-trees.csv`.
--- csv_report_builder.py
+++ csv_report_builder.py
@@ -7,56 +7,60 @@ import sys
 # Check for Correct Usage of Command Line
 if (len(sys.argv) < 3):
     print("Incorrect Usage:")
-    print("   argument 1 - name of CSV file to open")
-    print("   argument 2 - column names of CSV file to be written to new CSV (spearted by comman, in order)")
+    print("   python3 csv_report_builder.py filename colnames")
+    print("   filename - name of CSV file to open (i.e.: trees.csv)")
+    print("   colnames - column names of CSV file to be written to new CSV (i.e.: name,age,sex)")
+    exit()
 
-# Get Full Path to Current Working Directory
-currentWorkingDirectory = os.path.abspath(os.path.dirname(__file__))
+# CONSTANTS
+CURRENTWORKINGDIR = os.path.abspath(os.path.dirname(__file__)) # Get Full Path to Current Working Directory
+FILENAME = sys.argv[1] # Get the filename of the CSV that needs to be read from the user's command line argument
+COLNAMES = sys.argv[2]
 
 # Check that CSV filename specified by user exists
-if not (os.path.isfile(currentWorkingDirectory + '/' + sys.argv[1])):
+if not (os.path.isfile(CURRENTWORKINGDIR + '/' + FILENAME)):
     print("ERROR:")
-    print(sys.argv[1] + " does not exist within " + currentWorkingDirectory)
+    print(FILENAME + " does not exist within " + CURRENTWORKINGDIR)
     print("Place the CSV file in this directory and try again.")
     exit()
 
-# Create file[ath for output specified by user
-# The name of the file will be identical to the original file with 'output_' and the date the report is being generated ammended to the beginning
-name_of_output = "output-" + datetime.today().strftime('%Y-%m-%d') + '-' + sys.argv[1]
-complete_name_of_output = currentWorkingDirectory + '/' + name_of_output
-
-# Get the user's list of column names from the CSV file from the command line
-user_entry = sys.argv[2]
-user_entry_list = user_entry.split(",")
+# Create filepath for output specified by user
+# The name of the file will be identical to the original file with 'output-YYYY-mm-dd' appended to the beginning of the filename
+#   YYYY-mm-dd is the current date 
+name_of_output = "output-" + datetime.today().strftime('%Y-%m-%d') + '-' + FILENAME
+complete_name_of_output = CURRENTWORKINGDIR + '/' + name_of_output
 
 # Create a file with the generated filename
 file_output = open(complete_name_of_output, "w")
 
+# Create a list of column names that need to be pulled out from the original CSV file
+list_of_colnames_to_output = COLNAMES.split(",")
+
 # Initialize a variable to store the string that is to be written to the new file
 string_to_write_to_file = ''
 
-# Open given file
-with open(currentWorkingDirectory + '/' + sys.argv[1]) as csv_file:
+# Open original CSV file passed in by the user
+with open(CURRENTWORKINGDIR + '/' + FILENAME) as csv_file:
     csv_file_reader = csv.reader(csv_file)
     data = [row for row in csv_file_reader]
 
 # Create list to store the indexes of the columns the user wants to write into a new CSV file
-list_of_user_requested_cols = []
+list_of_col_indexes_matching_requested_cols = []
 
-for colName in user_entry_list: # Loop through every column name specified by the user from the command line
+for colName in list_of_colnames_to_output: # Loop through every column name specified by the user from the command line
     for col, name in enumerate(data[0]): # Loop through the header row in the original CSV file
 
         # If we find a match of a column name in the original CSV file and a name from the user's command line argument
         # Store the column index where the data is stored in the file
         if colName.strip() == name.replace('"', '').strip():
-            list_of_user_requested_cols.append(col)
+            list_of_col_indexes_matching_requested_cols.append(col)
 
 # For every row in the CSV file, select the columns specificed by the user
 for indx_row, row in enumerate(data):
-    for indx_col, user_index in enumerate(list_of_user_requested_cols):
+    for indx_col, user_index in enumerate(list_of_col_indexes_matching_requested_cols):
 
         # If this is the last column in the row, don't add a comma at the end
-        if indx_col == len(user_entry_list) - 1:
+        if indx_col == len(list_of_colnames_to_output) - 1:
             string_to_write_to_file += row[int(user_index)].strip()
         else:
             string_to_write_to_file += row[int(user_index)].strip() + ','
